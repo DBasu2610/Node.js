109444ec0dd23a55bfa54f0ffe9023eccb7eca4f
DBasu2610 <basudebadrita94@gmail.com>
2024-10-24 12:19:47 +0530

    changes made

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require("http");
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminData = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+const path = require('path');
+const rootDir = require('./util/path');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoute); 
+
+app.use((req,res,next)=> {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+
+app.listen(3000);
