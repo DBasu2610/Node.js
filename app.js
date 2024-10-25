3bc212b219fc98bab63c2b80502ce5dd113c3d17
DBasu2610 <basudebadrita94@gmail.com>
2024-10-25 11:40:22 +0530

    handlebars added

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 const path = require('path');
 const rootDir = require('./util/path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine','pug');
+app.engine('handlebars',expressHbs());
+app.set('view engine','handlebars');
 app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,7 +19,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoute); 
 
 app.use((req,res,next)=> {
-    res.status(404).render('404',{docTitle: 'Page Not Found'});
+    res.status(404).render('404',{pageTitle: 'Page Not Found'});
 });
 
 
