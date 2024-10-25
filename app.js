a88146fd0c42f8f00f1b3deff0b54354a9b715c2
DBasu2610 <basudebadrita94@gmail.com>
2024-10-25 17:29:01 +0530

    ejs added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const expressHbs = require('express-handlebars');
 const app = express();
 
 app.engine('handlebars',expressHbs());
-app.set('view engine','handlebars');
+app.set('view engine','ejs');
 app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended: false}));
